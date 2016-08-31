cef48b13b66f72b198d436fd6a00069062b3b95d
Arthur Kunkle <agkunkle@gmail.com>
2016-08-30 22:42:40 -0400

    angr script

---
+++ bitpuz-angr.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import angr
+import simuvex
+import logging
+from IPython import embed
+
+def main():
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('angr.path_group').setLevel(logging.DEBUG)
+
+    # Load the binary. This is a 64-bit C++ binary, pretty heavily obfuscated.
+    p = angr.Project('bitpuzzle')
+
+    # This block constructs the initial program state for analysis.
+    # Because we're going to have to step deep into the C++ standard libraries
+    # for this to work, we need to run everyone's initializers. The full_init_state
+    # will do that. In order to do this peformantly, we will use the unicorn engine!
+    st = p.factory.full_init_state(args=['./bitpuzzle'], add_options=simuvex.o.unicorn, remove_options={simuvex.o.LAZY_SOLVES})
+
+    #
+    # Set up the stdin symbolic input:
+    #
+    # It's reasonably easy to tell from looking at the program in IDA that the input will
+    # be 33 bytes long, and the last byte is a newline.
+
+    # Constrain the first 32 bytes to be non-null and non-newline:
+    for _ in xrange(32):
+        k = st.posix.files[0].read_from(1)
+        st.se.add(k != 0)
+        st.se.add(k != 10)
+
+    # Constrain the last byte to be a newline
+    k = st.posix.files[0].read_from(1)
+    st.se.add(k == 10)
+
+    # Reset the symbolic stdin's properties and set its length.
+    st.posix.files[0].seek(0)
+    st.posix.files[0].length = 33
+
+
+    # Construct a path group to perform symbolic execution.
+    # avoiding error blocks and finding success block
+    pg = p.factory.path_group(st)
+    pg.explore(find=0x080486b3, avoid=[0x0804856a,0x080486c1])
+
+    # Get the stdout of the path in our explored path group
+    for pp in pg.found:
+        print pp.state.posix.dumps(0)
+
+    # Runs in about 15 seconds!
+
+if __name__ == "__main__":
+    main()
